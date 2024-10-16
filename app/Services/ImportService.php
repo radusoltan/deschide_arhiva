7fb87134e5211508716c56196d06137e454c19b4
Radu Soltan <radusoltan@gmail.com>
2024-10-16 13:55:31 +0300

    update import (page 2); updated Article Import (improve ru lang);

--- app/Services/ImportService.php
+++ app/Services/ImportService.php
@@ -48,8 +48,8 @@ class ImportService {
         $localAuthor = Author::where('old_number', $author->object()->id)->first();
         if(!$localAuthor) {
             $localAuthor = Author::create([
-                'first_name' => $author->object()->firstName,
-                "last_name" => $author->object()->lastName,
+                'first_name' => $author->object()->firstName === "Petru" && app()->getLocale() === 'ru' ? 'Петру' : 'Petru',
+                "last_name" => $author->object()->lastName === "Bogatu" && app()->getLocale() === 'ru' ? "Богату" : "Bogatu",
                 "full_name" => $author->object()->firstName . ' ' . $author->object()->lastName,
                 'slug' => Str::slug($author->object()->firstName . ' ' . $author->object()->lastName),
                 'old_number' => $author->object()->id,
